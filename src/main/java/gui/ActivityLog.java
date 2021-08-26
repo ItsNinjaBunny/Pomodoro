8ba12171282d0d05e6746f183b6caae5800e76fe
braydenwonglee <braydenlwong@gmail.com>
2021-08-26 01:19:51 -0600

    I hate myself

--- src/main/java/gui/ActivityLog.java
+++ src/main/java/gui/ActivityLog.java
@@ -1,20 +1,26 @@
 package gui;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.swing.DefaultListModel;
-import javax.swing.JFrame;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.ListSelectionModel;
+import javax.swing.*;
 
 import mongoDB.Mongo;
 
 public class ActivityLog {
 	
-	private String username;
-	private Mongo mongo = new Mongo();
+	private final String username;
+	private final Mongo mongo = new Mongo();
+	private final DefaultListModel actList = new DefaultListModel();
+
+	private int m = 0;
+	private int h = 0;
+	private int d = 0;
+
+	private String days, hours, minutes;
 	
 	public String getUsername() {
 		return this.username;
@@ -28,34 +34,101 @@ public class ActivityLog {
 	@SuppressWarnings("rawtypes")
 	public void log(JPanel mainPanel) {
 		JPanel activityLog = new JPanel(null);
+		List<String> sessionTimes = new ArrayList<>();
 		
 		activityLog.setPreferredSize(new Dimension(500, 500));
-		JList list = getLogs();
+		JList list = getLogs(sessionTimes);
 		list.setVisibleRowCount(5);
 		list.setLayoutOrientation(JList.VERTICAL);
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);	
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+
+
+		for (String sessionTime : sessionTimes) {
+			m += (int) Double.parseDouble(sessionTime) % 60;
+			if(m >= 60) {
+				h += 1;
+				m = Math.abs(m - 60);
+			}
+			if(h >= 24) {
+				d += 1;
+				h = Math.abs(24 - h);
+			}
+			//System.out.println("hours: " + h + " minutes: " + m);
+		}
+		hours = String.valueOf(h);
+		minutes = String.valueOf(m);
+		days = String.valueOf(d);
+
+
 		JScrollPane scroller = new JScrollPane(list);
 		scroller.setBounds(10, 10, 480, 300);
 		scroller.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		scroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		activityLog.setPreferredSize(new Dimension(500, 500));
-		
-		activityLog.add(scroller);	
-		
-		
+
+
+		JLabel session = new JLabel();
+		session.setBounds(30, 310, 200, 30);
+		session.setText("days: " + days + " hours: " + hours + " minutes: " + minutes);
+
+		JButton update = new JButton("update");
+		update.setBounds(350, 310, 68, 30);
+		update.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				for(int i = 0; i < sessionTimes.size(); i++) {
+					System.out.println(sessionTimes.get(i));
+					sessionTimes.remove(i);
+				}
+				actList.clear();
+
+				list.removeAll();
+				mongo.getLogs(getUsername(), actList, sessionTimes);
+				m = 0;
+				h= 0;
+				d = 0;
+
+				for (String sessionTime : sessionTimes) {
+					m += (int) Double.parseDouble(sessionTime) % 60;
+					if(m >= 60) {
+						h += 1;
+						m = Math.abs(m - 60);
+					}
+					if(h >= 24) {
+						d += 1;
+						h = Math.abs(24 - h);
+					}
+					//System.out.println("hours: " + h + " minutes: " + m);
+				}
+				minutes = String.valueOf(m);
+				hours = String.valueOf(h);
+				days = String.valueOf(d);
+
+				session.setText("days: " + days + " hours: " + hours + " minutes: " + minutes);
+				activityLog.revalidate();
+			}
+		});
+
+		activityLog.add(update);
+		activityLog.add(session);
+		activityLog.add(scroller);
+
 		mainPanel.add(activityLog, "log");
 	}
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	
-	private JList getLogs() {
-		DefaultListModel actList = new DefaultListModel();
-		mongo.getLogs(username, actList);
+	private JList getLogs(List<String> sessionTimes) {
+
+		mongo.getLogs(username, actList, sessionTimes);
 		return new JList(actList);
 	}
 	
 	public static void main(String[] args) {
-		
+
 		JFrame frame = new JFrame();
 		frame.setBounds(100, 100, 500, 500);
 		
--- src/main/java/mongoDB/Mongo.java
+++ src/main/java/mongoDB/Mongo.java
@@ -1,10 +1,9 @@
 
 package mongoDB;
 
-import java.util.Iterator;
-
 import javax.swing.DefaultListModel;
 
+import com.mongodb.client.model.Sorts;
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
@@ -12,6 +11,8 @@ import com.mongodb.MongoClientURI;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+
+import java.util.List;
 
 public class Mongo {
 	
@@ -24,30 +25,25 @@ public class Mongo {
 		final String connection = "mongodb://user_1:Passw0rd1@pomodoro-shard-00-01.jnyc8.mongodb.net:27017/users?ssl=true&replicaSet=atlas-nqzfih-shard-0&authSource=admin&retryWrites=true";
 		
 		MongoClientURI uri = new MongoClientURI(connection);
-		MongoClient mongoClient = new MongoClient(uri);
-		
-		return mongoClient;
+
+		return new MongoClient(uri);
 	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public DefaultListModel getLogs(String username, DefaultListModel documents) {
+	public void getLogs(String username, DefaultListModel documents, List<String> sessionTimes) {
 		
 		MongoClient mongoClient = connectDatabase();
 		MongoDatabase database = mongoClient.getDatabase(username);
 		MongoCollection<Document> collection = database.getCollection("logs");
 		
-		FindIterable<Document> iterDoc = collection.find();
-		Iterator it = iterDoc.iterator();
-		
-		while(it.hasNext()) {
-			Document tempDoc = (Document) it.next();
-			
-			Document doc = new Document();
-			doc.append("SessionID", tempDoc.get("sessionID")).append("sessionLength", tempDoc.get("sessionLength"));
-			documents.addElement(doc);
+		FindIterable<Document> iterDoc = collection.find().sort(Sorts.ascending("sessionID"));
+
+		for (Document tempDoc : iterDoc) {
+			documents.addElement("Session ID: " + String.valueOf(tempDoc.get("sessionID") + "\t Session Time: " + String.valueOf(tempDoc.get("sessionLength"))));
+			String session = String.valueOf(tempDoc.get("sessionLength")).replace(":", ".");
+			sessionTimes.add(session);
 		}
 		
 		mongoClient.close();
-		return documents;
 	}
 }
